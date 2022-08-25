7494142a9478273a32f9c3279f0e4904c4e24c84
Arthur José <57883830+Arthurit0@users.noreply.github.com>
2022-08-25 18:23:41 -0300

    Q4 Done

--- Lista de Exercícios 1 - Revisão de C/Q4.c
+++ Lista de Exercícios 1 - Revisão de C/Q4.c
@@ -1,19 +1,30 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
-    int tam, num, j;
-    list *v;
-
+    int tam, num, j, *v, excl_new, incl, excl;
     printf("Digite o tamanho do vetor: ");
     scanf("%d", &tam);
 
-    v = (list *)malloc(sizeof(list));
+    v = (int *)malloc(sizeof(int));
 
     for (int i = 0; i < tam; i++) {
         printf("Digite um valor para a posição %d do vetor: ", i);
         scanf("%d", &num);
-        }
+        v[i] = num;
+    }
+
+    incl = v[0];
+
+    for (int i = 1; i < tam; i++) {
+        excl_new = (incl > excl) ? incl : excl;
+
+        incl = excl + v[i];
+        excl = excl_new;
+    }
+
+    printf("Maior Soma: %d", incl > excl ? incl : excl);
 
     return 0;
 }
