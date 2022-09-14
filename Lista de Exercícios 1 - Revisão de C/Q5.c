80e17b41e6d0d1415ecac6bdbaa9b74065a74d64
Arthur José <arthur.jose27@gmail.com>
2022-09-14 14:34:02 -0300

    Q5

--- Lista de Exercícios 1 - Revisão de C/Q5.c
+++ Lista de Exercícios 1 - Revisão de C/Q5.c
@@ -11,25 +11,57 @@ int sumArr(int *v, int size) {
 }
 
 int main(int argc, char const *argv[]) {
-    int n, menorNum, *v, size;
+    int n, menorNum, *v, size, soma = 0, bigBreak = 0;
 
     printf("Digite o valor de n: ");
     scanf("%d", &n);
     size = n;
-    menorNum = n / 2;
+    menorNum = (n / 2);
 
-    v = (int *)malloc(sizeof(int) * n);
+    v = (int *)malloc(sizeof(int) * size);
 
-    printf("[ ");
-    for (int i = 0; i < size; i++) {
-        *(v + i) = 1;
+    while (size >= menorNum) {
+        for (int k = 0; k < size; k++) {
+            v[k] = 1;
+        }
+        for (int i = 0; i <= size; i++) {
+            for (int k = 0; k < size; k++) {
+                v[k] = 1;
+            }
 
-        i + 1 == size ? printf("1 ]") : printf("1, ");
-    }
+            for (int j = 0; j <= size; j++) {
+                // Soma valores
+                for (int k = 0; k < size; k++) {
+                    soma += *(v + k);
+                }
 
-    (int *)realloc(v, sizeof(int) * --size);
+                if (soma == n) {
+                    // Printa valores:
+                    for (int k = 0; k < size; k++) {
+                        printf("%d, ", *(v + k));
+                    }
+                    printf("\n");
 
-    for (int i = 0; i < size; i++) {
-        v[i] = v[i]++;
+                    if (size == n) {
+                        bigBreak = 1;
+                        break;
+                    }
+                }
+                soma = 0;
+                if (i + j < size) {
+                    *(v + i + j) += 1;
+                } else {
+                    break;
+                }
+            }
+            if (bigBreak) {
+                bigBreak = 0;
+                soma = 0;
+                break;
+            }
+        }
+        size--;
+        free(v);
+        v = (int *)malloc(sizeof(int) * size);
     }
 }
